56cb4def56c062469fef8daece0c14d385c378dc
Tara <tarawijayagunawarana@ITS042011.local>
2025-07-22 09:56:08 +1000

    [GOVCMSD10-1355] Mark jQuery UI Resizable module as obsolete

--- src/Modules/Lifecycle.php
+++ src/Modules/Lifecycle.php
@@ -26,8 +26,8 @@ class Lifecycle {
     'aggregator',
     'block_inactive_users',
     'event_log_track_ui',
-    'panels', 
-    'permissions_by_term'
+    'panels',
+    'jquery_ui_resizable',
   ];
 
   /**
